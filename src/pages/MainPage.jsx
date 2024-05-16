7e7efe6e1b63a9f963274320106ee2a756a87ca8
Vera869 <verea8910@gmail.com>
2024-05-16 20:19:42 +0700

    +

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,11 +2,10 @@ import "./PagesStyled.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getCountries } from "../api.jsx";
 import { setCountries, setCountryName, setIsLoading } from "../store/Slice.jsx";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Main = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const dataCountries = useSelector((state) => state.countries.countries);
   const isLoading = useSelector((state) => state.countries.isLoading);
 
@@ -25,17 +24,12 @@ export const Main = () => {
       <h2 className="countries__header">All countries</h2>
       <ul className="countries__list">
         {dataCountries?.map((country, index) => {
-          //console.log(country.name.common);
           const countryName = `${country.name.common}`
           const imgUrl = `${country.flags.png}`
           return (
             <li key={index} className="countries__item">
               <img src={imgUrl} className="item__img"/>
-              <Link className="item__name" onClick={() => {
-                dispatch(setCountryName(country.name.common));
-               // navigate("/:countryName")
-              } 
-                } to={`/${countryName}`}>
+              <Link className="item__name" onClick={() => dispatch(setCountryName(country.name.common))} to={`/${countryName}`}>
                   {countryName}
                   </Link >
             </li>
