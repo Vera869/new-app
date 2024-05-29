1b5ccf384171fb421d4496129d29d8b0ab532e53
Vera869 <verea8910@gmail.com>
2024-05-29 15:05:15 +0700

    +

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import "./HeaderStyled.css";
 export const HeaderBox = () => {
   return (
     <div className="HeaderBox">
-      <img className="HeaderBox__img" src="./img/iconGlobe.png" alt="countries"/>
+      <img className="HeaderBox__img" src="../../src/components/header/img/iconGlobe.png" alt="countries"/>
       <h1 className="HeaderBox__header">COUNTRIES </h1>
     </div>
   );
