980e57e59e7fb917a404381ba8925a17a2239c2a
Vera869 <verea8910@gmail.com>
2024-06-22 17:22:14 +0700

    + navigate

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import "./HeaderStyled.css";
 
 export const HeaderBox = () => {
+  const navigate = useNavigate();
   return (
-    <div className="HeaderBox">
+    <div className="HeaderBox" onClick={()=> navigate("/")}>
       <img className="HeaderBox__img" src="../../src/components/header/img/iconGlobe.png" alt="countries"/>
       <h1 className="HeaderBox__header">COUNTRIES </h1>
     </div>
