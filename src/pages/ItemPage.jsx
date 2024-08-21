d262729b326a4f3bdfd0a7a32ece27eba506694c
Vera869 <verea8910@gmail.com>
2024-08-21 17:13:56 +0700

    fix

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,18 +1,27 @@
-import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import  "./PagesStyle.css";
 
 export const ItemPage = () => {
   const navigate = useNavigate();
   
   const [isLoad, setIsLoad] = useState(true);
-  const currentItem = useSelector((state) => state.items.item[0]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [currentItem, setCurrentItem] = useState({});
 
-  //Показываем прелодер 1,5 секунды, пока загружается контент с сервера
-  setTimeout(() => {
-    setIsLoad(false);
-  }, 1500);
+  const currentItemId = localStorage.getItem("id");
+
+  useEffect(() => {
+    //Получаем данные с сервера и сохраняем в state
+    fetch(`https://fakestoreapi.com/products/${currentItemId}`)
+      .then(res => res.json())
+      .then(json =>{
+        setCurrentItem(json);
+        setIsLoad(false);
+      })
+      .catch(error => setErrorMessage(error.message));
+
+  }, [currentItemId]);
  
   const hendleNavigate = () => {
     navigate("/");
@@ -24,15 +33,16 @@ export const ItemPage = () => {
         <div className="item"> 
           {currentItem === undefined ? <>
             <h2 className="item-header">Something went wrong, try again</h2>
+            <p className="item-header">{errorMessage}</p>
             <button className="item-button" onClick={hendleNavigate}>Go to home page</button></> : <>
             <h2 className="item-header">{currentItem.title}</h2>
             <img className="item-image" src={`${currentItem.image}`}/>
             <p className="item-descriptions">category: {currentItem.category}</p>
-            <p className="item-descriptions">price: {currentItem.price}</p>
-            <p className="item-descriptions">raiting count: {currentItem.rating.count}</p>
-            <p className="item-descriptions">raiting rate: {currentItem.rating.rate}</p>
+            <p className="item-descriptions">price: <span className="span-numbers">{currentItem.price}</span></p>
+            <p className="item-descriptions">raiting count: <span className="span-numbers">{currentItem.rating.count}</span></p>
+            <p className="item-descriptions">raiting rate: <span className="span-numbers">{currentItem.rating.rate}</span></p>
             <p className="item-descriptions">{currentItem.description}</p>
-            <button className="item-button" onClick={hendleNavigate}>Go to home page</button>;
+            <button className="item-button" onClick={hendleNavigate}>Go to home page</button>
           </>}
         </div>}
       
